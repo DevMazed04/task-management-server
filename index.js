0f3b5113bee3a82797896fc65fdae1866bd65b1a
DevMazed04 <mdmazedislam2684@gmail.com>
2022-12-29 02:40:44 +0600

    use the environment variable by creating .env file

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vfwjfcr.mongodb.net/?retryWrites=true&w=majority`;
 
 app.get("/", async (req, res) => {
    res.send("Task Management API is running");
