262219a22e62ec8ee368f29da7517e08a61a8c75
DevMazed04 <mdmazedislam2684@gmail.com>
2022-12-29 03:09:28 +0600

    inserted tasks data to taskManagement database in mongodb

--- index.js
+++ index.js
@@ -12,15 +12,33 @@ app.use(express.json());
 // database connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vfwjfcr.mongodb.net/?retryWrites=true&w=majority`;
 
-app.get("/", async (req, res) => {
-   res.send("Task Management API is running");
-});
-
 const client = new MongoClient(uri, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
    serverApi: ServerApiVersion.v1,
 });
+
+app.get("/", async (req, res) => {
+   res.send("Task Management API is running");
+});
+
+const run = async () => {
+   try {
+      // Database Collections
+      const tasksCollection = client.db("taskManagement").collection("tasks");
+
+      // Tasks API
+      app.get("/tasks", async (req, res) => {
+         const query = {};
+         const tasks = await tasksCollection.find(query).toArray();
+         res.send(tasks);
+      });
+
+   } finally {
+   }
+};
+run().catch((err) => console.log(err));
+
 
 app.listen(port, () => {
    console.log("Task Management server is running on port", port);
